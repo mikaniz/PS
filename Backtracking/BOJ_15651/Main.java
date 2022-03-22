016c3d03f4478ffe8a4e70bbafcbc1be7b974de5
mikanji <k515jina@gmail.com>
2022-03-23 00:01:46 +0900

    Add Backtracking/BOJ_15651

---
+++ Backtracking/BOJ_15651/Main.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	static BufferedWriter bw;
+	static int N;
+	static int M;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		find(new int[M], 0);
+		bw.flush();
+		bw.close();
+	}
+	
+	public static void find(int[] bucket, int n) throws IOException {
+		if (n == M) {
+			for (int i = 0; i < M; i++) {
+				if (i != 0) {
+					bw.write(" ");
+				}
+				bw.write(String.valueOf(bucket[i]));
+			}
+			bw.newLine();
+			return;
+		}
+		
+		for (int i = 1; i <= N; i++) {
+			bucket[n] = i;
+			find(bucket, n + 1);
+		}
+	}
+
+}
